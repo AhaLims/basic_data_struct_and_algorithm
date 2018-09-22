7aa3f396ced184a5ebc60d295f274d1661a6e0b7
AhaLims <915673037@qq.com>
2018-09-22 22:37:00 +0800

    add LIS(O^2) in dynamic process

---
+++ dynamic process/LIS_O(n^2).cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+//LIS O(n^2)
+const int maxn = 1000;//set by the question
+int n;//number amount
+int number[maxn];
+int sub_number[maxn];
+
+void LIS(int & maxLen, int & index)
+{
+	maxLen = 1;
+	index = 0;
+	sub_number[0] = 1;
+	for (int i = 1; i < n; i++)
+	{
+		sub_number[i] = 1;
+		for (int j = 0; j < i; j++)
+		{
+			if (number[j] < number[i])sub_number[i] = sub_number[j] + 1;//state transition equation
+		}
+		if (sub_number[i] > maxLen)
+		{
+			maxLen = sub_number[i];
+			index = i;
+		}
+	}
+}
+
+int main()
+{
+	cin >> n;
+	for (int i = 0; i<n; i++)cin >> number[i];
+	int maxL, index;
+	LIS(maxL, index);
+	cout << maxL << endl;
+}
